d4688135f5a03e7af3994a1ba093a227a24b1fc2
Aleroms <santiagomorales333@yahoo.com>
2022-10-07 23:56:31 -0700

    learned about data()

---
+++ section-2-1-STARTER/app.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+  data() {
+    return {
+      firstname: "Santiago",
+      lastname: "Morales",
+    };
+  },
+}).mount("#app");
+
+//data 
+//  - A function that returns the initial reactive state for the component instance.
+//https://vuejs.org/api/options-state.html#data
+
