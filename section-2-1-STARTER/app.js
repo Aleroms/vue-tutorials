9615f5c7ca05bc454e46705cd427ae2a5e8baea8
Aleroms <santiagomorales333@yahoo.com>
2022-10-08 10:36:43 -0700

    learned about binding, directives and methods

--- section-2-1-STARTER/app.js
+++ section-2-1-STARTER/app.js
@@ -1,11 +1,21 @@
-Vue.createApp({
+const vm = Vue.createApp({
   data() {
     return {
       firstname: "Santiago",
       lastname: "Morales",
+      url: 'https://google.com'
     };
   },
+  methods: {
+    fullName(){
+      return this.firstname + ' ' + this.lastname;
+    }
+  }
 }).mount("#app");
+
+// setTimeout(()=>{
+//   vm.firstname = 'bob';
+// },2000);
 
 //data 
 //  - A function that returns the initial reactive state for the component instance.
