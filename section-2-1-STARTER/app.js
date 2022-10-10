27596da597eff40e8aa6bba95b5041c564b6d369
Aleroms <santiagomorales333@yahoo.com>
2022-10-10 16:39:26 -0700

    learned computed properties, binding, and events

--- section-2-1-STARTER/app.js
+++ section-2-1-STARTER/app.js
@@ -2,22 +2,47 @@ const vm = Vue.createApp({
   data() {
     return {
       firstname: "Santiago",
+      middleName: "",
       lastname: "Morales",
-      url: 'https://google.com'
+      url: "https://google.com",
+      raw_url: '<a href="https://google.com">Google</a>',
+      age: 20,
     };
   },
   methods: {
-    fullName(){
-      return this.firstname + ' ' + this.lastname;
+    
+    increment() {
+      this.age++;
+    },
+    updateLastName(msg, event) {
+      //console.log(msg);
+      this.lastname = event.target.value;
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value;
+    },
+  },
+  computed: {
+    fullName() {
+      console.log("full name computed property method was called");
+      return `${this.firstname} ${this.middleName} ${this.lastname}`;
+    }
+  },
+  watch: {
+    age(newVal, oldVal){
+      setTimeout(()=>{
+        this.age = 20;
+      },1000);
     }
   }
-}).mount("#app");
+});
+
+vm.mount("#app");
 
 // setTimeout(()=>{
 //   vm.firstname = 'bob';
 // },2000);
 
-//data 
+//data
 //  - A function that returns the initial reactive state for the component instance.
 //https://vuejs.org/api/options-state.html#data
-
