e16c349d278bee20a6618401f52d9cf038456350
Aleroms <santiagomorales333@yahoo.com>
2022-10-14 13:36:50 -0700

    callbacks,events,props

--- components/src/App.vue
+++ components/src/App.vue
@@ -13,12 +13,20 @@ export default {
     Greeting,
     User,
   },
+  methods: {
+    updateAge(num){
+      this.age++;
+    },
+    updateAgeCB(num){
+      return this.age += num;
+    }
+  }
 };
 </script>
 
 <template>
   <Greeting :age="age" />
-  <User :age="age" />
+  <User :age="age" @age-change="updateAge" :ageChangeFn="updateAgeCB"/>
   <p>{{ msg }}</p>
 </template>
 
--- components/src/components/User.vue
+++ components/src/components/User.vue
@@ -1,11 +1,35 @@
 <template>
+  <button type="button" @click="onClickAge">age up</button>
+  <button type="button" @click="ageChangeFn(3)">age up cb</button>
   <p>the user is {{ age }} years old</p>
+  <p>{{ ageDoubled }}</p>
 </template>
 
 <script>
 export default {
   name: "User",
-  props: ["age"],
+  props: {
+    age: {
+      type: Number,
+      //required: true,
+      //default: 20
+      validator(val) {
+        return val < 130;
+      },
+    },
+    ageChangeFn: Function,
+  },
+  emits: ["age-change"],
+  computed: {
+    ageDoubled() {
+      return this.age * 2;
+    },
+  },
+  methods: {
+    onClickAge() {
+      this.$emit("age-change", 3);
+    },
+  },
 };
 </script>
 
