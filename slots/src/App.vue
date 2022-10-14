d76cf16fc839d68e5f78ea1026d16892f132e579
Aleroms <santiagomorales333@yahoo.com>
2022-10-14 14:04:40 -0700

    slots, named slots, events/callbacks

---
+++ slots/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import AppForm from "./components/Form.vue";
+export default {
+  name: "App",
+  components: { AppForm },
+};
+</script>
+
+<template>
+  <AppForm>
+    <template v-slot:help>this is help text</template>
+    <template v-slot:fields>
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="password" />
+      <input type="text" placeholder="email" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">submit</button>
+    </template>
+    <p>dummy txt</p>
+  </AppForm>
+  <AppForm>
+    <template v-slot:help>Contact form</template>
+    <template v-slot:fields>
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="message" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">submit</button>
+    </template>
+  </AppForm>
+</template>
