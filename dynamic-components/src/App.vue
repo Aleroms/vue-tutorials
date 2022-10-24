784617332193351a4d6ad7d25fdd49f0376d388c
Aleroms <santiagomorales333@yahoo.com>
2022-10-24 11:41:42 -0700

    i forgot but pushing :0

---
+++ dynamic-components/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      flag: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <button type="button" @click="flag = !flag">Toggle</button>
+  <transition>
+    <h2 v-if="flag">text inside</h2>
+  </transition>
+</template>
+
+<style></style>
---
+++ dynamic-components/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//this project will focus on dynamic components
+import './assets/main.css'
+
+createApp(App).mount('#app')
