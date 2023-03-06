b589e675b610fa1997b5b154ff76279f91f8a80d
Santiago <santiagomorales333@yahoo.com>
2023-03-06 15:02:27 -0800

    perceived performance

--- music-app/music/src/views/Song.vue
+++ music-app/music/src/views/Song.vue
@@ -136,20 +136,21 @@ export default {
       sort: "1",
     };
   },
-  async created() {
-    const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
+  async beforeRouteEnter(to, from, next) {
+    const docSnapshot = await songsCollection.doc(to.params.id).get();
+    next((vm) => {
+      if (!docSnapshot.exists) {
+        this.$router.push({ name: "home" });
+        return;
+      }
 
-    if (!docSnapshot.exists) {
-      this.$router.push({ name: "home" });
-      return;
-    }
+      const { sort } = vm.$route.query;
 
-    const { sort } = this.$route.query;
+      vm.sort = sort === "1" || sort === "2" ? sort : "1";
 
-    this.sort = sort === "1" || sort === "2" ? sort : "1";
-
-    this.song = docSnapshot.data();
-    this.getComments();
+      vm.song = docSnapshot.data();
+      vm.getComments();
+    });
   },
   methods: {
     ...mapActions(usePlayerStore, ["newSong"]),
