691b0872df14e5c9267ee6b7ee0e89b96f88ceb7
Santiago <santiagomorales333@yahoo.com>
2023-03-15 16:53:10 -0700

    teleport component

--- section-22-5-STARTER/src/App.vue
+++ section-22-5-STARTER/src/App.vue
@@ -1,41 +1,15 @@
 <template>
   <div id="app">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ante
-      dignissim, vulputate lectus id, lobortis justo. Aliquam sit amet
-      scelerisque risus, non porta lorem. Maecenas ornare odio purus, id
-      consequat ligula aliquam sit amet. Nam sit amet efficitur sem. Etiam
-      finibus erat id ligula ornare fringilla. Suspendisse potenti. Duis eu
-      molestie felis. Suspendisse eu vestibulum turpis. Pellentesque venenatis
-      lacus lectus, a fringilla est pretium ac. Vestibulum ante ipsum primis in
-      faucibus orci luctus et ultrices posuere cubilia curae; Phasellus mattis
-      massa id pretium efficitur. Ut consequat elit sed dapibus fermentum.
-      Suspendisse sed ipsum vel arcu tincidunt finibus ut sed est. Suspendisse
-      venenatis pharetra enim, gravida aliquam risus sagittis et. Nullam gravida
-      fringilla tortor. Quisque ornare gravida arcu.
-    </p>
-    <div class="mt-2 ml-2">
-      <button type="button" class="btn btn-primary" @click="open = true">
-        Open
-      </button>
-    </div>
-
-    <app-modal :show="open" @hide="open = false" />
+    <Action />
   </div>
 </template>
 
 <script>
-import AppModal from "@/components/Modal.vue";
-
+import Action from "./components/Action.vue";
 export default {
   name: "App",
   components: {
-    AppModal,
-  },
-  data() {
-    return {
-      open: false,
-    };
+    Action,
   },
 };
 </script>
---
+++ section-22-5-STARTER/src/components/Action.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mt-2 ml-2">
+    <button type="button" class="btn btn-primary" @click="open = true">
+      Open
+    </button>
+    <Teleport to="body"
+      ><app-modal :show="open" @hide="open = false"
+    /></Teleport>
+  </div>
+</template>
+
+<script>
+import AppModal from "@/components/Modal.vue";
+export default {
+  name: "AppAction",
+  components: {
+    AppModal,
+  },
+  data() {
+    return {
+      open: false,
+    };
+  },
+};
+</script>
