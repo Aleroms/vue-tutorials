e57946a99cb5dcef17358f729906e0976fdec10c
Santiago <santiagomorales333@yahoo.com>
2023-03-15 16:39:04 -0700

    idk man

---
+++ section-22-5-STARTER/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ante
+      dignissim, vulputate lectus id, lobortis justo. Aliquam sit amet
+      scelerisque risus, non porta lorem. Maecenas ornare odio purus, id
+      consequat ligula aliquam sit amet. Nam sit amet efficitur sem. Etiam
+      finibus erat id ligula ornare fringilla. Suspendisse potenti. Duis eu
+      molestie felis. Suspendisse eu vestibulum turpis. Pellentesque venenatis
+      lacus lectus, a fringilla est pretium ac. Vestibulum ante ipsum primis in
+      faucibus orci luctus et ultrices posuere cubilia curae; Phasellus mattis
+      massa id pretium efficitur. Ut consequat elit sed dapibus fermentum.
+      Suspendisse sed ipsum vel arcu tincidunt finibus ut sed est. Suspendisse
+      venenatis pharetra enim, gravida aliquam risus sagittis et. Nullam gravida
+      fringilla tortor. Quisque ornare gravida arcu.
+    </p>
+    <div class="mt-2 ml-2">
+      <button type="button" class="btn btn-primary" @click="open = true">
+        Open
+      </button>
+    </div>
+
+    <app-modal :show="open" @hide="open = false" />
+  </div>
+</template>
+
+<script>
+import AppModal from "@/components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppModal,
+  },
+  data() {
+    return {
+      open: false,
+    };
+  },
+};
+</script>
+
+<style src="bootstrap/dist/css/bootstrap.css"></style>
---
+++ section-22-5-STARTER/src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="modal" :style="{ display: show ? 'block' : 'none' }">
+    <div
+      class="modal-dialog"
+      style="z-index: 2000"
+      @keydown.esc="close"
+      tabindex="0"
+      ref="modal"
+    >
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Awesome Title</h5>
+          <button type="button" class="close" @click="close">
+            <span>&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Awesome content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop show"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppModal",
+  props: ["show"],
+  methods: {
+    close() {
+      console.log("emitting event");
+      this.$emit("hide");
+    },
+  },
+  watch: {
+    show(newVal) {
+      if (newVal) {
+        this.$refs.modal.focus();
+      }
+    },
+  },
+};
+</script>
+<style>
+.modal-dialog {
+  outline: 0;
+}
+</style>
